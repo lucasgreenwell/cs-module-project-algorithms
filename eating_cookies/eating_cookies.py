fcccbc6db237d1ccbe4a330c984dd4737f9859ee
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-07-19 21:55:08 -0600

    finishes brute force inefficient eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,21 @@
 Input: an integer
 Returns: an integer
 '''
+import itertools
 def eating_cookies(n):
     # Your code here
-
-    pass
+    #create a list containing 1,2,3 the number of times that n is divisible by and also create a counter variable
+    # create all permutations of that list and if the permutation adds up to n, then increment the counter
+    #return the counter
+    # possible_addends = []
+    # for i in range(0, n):
+    #     possible_addends.append(1)
+    # for i in range(n // 2):
+    #     possible_addends.append(2)
+    # for i in range(n // 3):
+    #     possible_addends.append(3)
+    # result = [seq for i in range(len(possible_addends), 0, -1) for seq in itertools.combinations(possible_addends, i) if sum(seq) == n]
+    # return len(result)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
