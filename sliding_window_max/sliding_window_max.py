5b69285599db1f77377911a257a6f1b445361780
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-07-20 11:13:20 -0600

    finishes sliding window in linear time

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,9 +3,29 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
+    # set up variables for the beginning and end of the window
+    #set up a result array
+    #loop through the array
+        #if the window can't keep moving break the loop
+        #slice the array
+        #push the maximum element in the slice
+        # iterate the window
+    #return the result array
+    beginning_of_the_window = 0
+    end_of_the_window = k
+    res = []
+    for num in nums:
+        if end_of_the_window > len(nums):
+            break
+        window = nums[beginning_of_the_window:end_of_the_window]
+        window.sort()
+        print(window)
+        res.append(window[-1])
+        beginning_of_the_window += 1
+        end_of_the_window += 1
+    return res
 
-    pass
+
 
 
 if __name__ == '__main__':
