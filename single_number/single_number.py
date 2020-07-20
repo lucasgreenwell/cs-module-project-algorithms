3b5e0947f5d6a06000c891ca77e35142998fe6d1
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-07-19 21:06:37 -0600

    linear solution to single number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,13 +3,18 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    for num in arr:
-        count = 0
-        for num_again in arr:
-            if num_again == num:
-                count += 1
-        if count < 2:
+    for num in  arr:
+        if arr.count(num) < 2:
             return num
+    # for num in arr:
+    #     count = 0
+    #     for num_again in arr:
+    #         if num_again == num:
+    #             count += 1
+    #     if count < 2:
+    #         return num
+
+
 
 
 if __name__ == '__main__':
